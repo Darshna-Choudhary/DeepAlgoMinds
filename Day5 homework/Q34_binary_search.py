0632df2ca451573cf81de563008d319615774d6d
Darshna-Choudhary <darshnachoudhary1823@gmail.com>
2026-06-05 22:04:52 +0530

    Q34 hw

---
+++ Day5 homework/Q34_binary_search.py
@@ -0,0 +1,12 @@
+def search(nums, target):
+        st = 0
+        end = len(nums) - 1
+        while st <= end:
+            mid = st + (end-st) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] > target:
+                end = mid - 1
+            else:
+                st = mid + 1
+        return -1
