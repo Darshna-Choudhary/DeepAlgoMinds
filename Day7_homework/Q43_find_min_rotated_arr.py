69dc0106d5cd4b98a4def38762ab9a2719ef96be
Darshna-Choudhary <darshnachoudhary1823@gmail.com>
2026-06-08 20:03:38 +0530

    Q43 hw

---
+++ Day7_homework/Q43_find_min_rotated_arr.py
@@ -0,0 +1,9 @@
+def findMin(nums):
+        st = 0; end = len(nums) - 1
+        while st < end:
+            mid = st + (end - st) // 2
+            if nums[mid] > nums[end]:
+                st = mid + 1
+            else:
+                end = mid
+        return nums[st]
