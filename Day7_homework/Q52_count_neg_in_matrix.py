d25746aeee7979eed2c95909aae5921514b9cba9
Darshna-Choudhary <darshnachoudhary1823@gmail.com>
2026-06-09 12:57:32 +0530

    Q52 hw

---
+++ Day7_homework/Q52_count_neg_in_matrix.py
@@ -0,0 +1,12 @@
+def countNegatives(grid):
+        n = len(grid)
+        m = len(grid[0])
+        row = n-1; col = 0
+        count = 0
+        while row >= 0 and col < m:
+            if grid[row][col] < 0:
+                count += m - col
+                row -= 1
+            else:
+                col += 1
+        return count
