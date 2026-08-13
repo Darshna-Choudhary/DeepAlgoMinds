0c3748f995d673e2e8fd532deb9f4abcfb983b08
Darshna <darshnachoudhary1823@gmail.com>
2026-08-13 18:35:15 +0530

    Update Pairwise_swap_linked_list.py

--- Pairwise_swap_linked_list.py
+++ Pairwise_swap_linked_list.py
@@ -22,3 +22,19 @@ def pairwiseSwap(head):
             return temp
         nxt = second.next
         return temp
+# ------------------------------------------------------------------------
+def pairwiseSwap(head):
+    if head is None or head.next is None:
+        return head
+    node = Node(Node)
+    node.next = head
+    prev = node
+        
+    while prev.next and prev.next.next:
+        first = prev.next
+        second = first.next
+        prev.next = second
+        first.next = second.next
+        second.next = first
+        prev = first
+    return node.next
